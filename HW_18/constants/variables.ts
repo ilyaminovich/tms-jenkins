5a8081e75bd4d12f006e24162018d6455bfe9f16
AlesyaBb <babich.alesia@gmail.com>
2024-05-15 02:34:34 +0300

    (hw_18): Integration API testing_edits

---
+++ HW_18/constants/variables.ts
@@ -0,0 +1,22 @@
+export const expectedObjForGet : {id: number, email: string, first_name: string, last_name: string, avatar: string} = {
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg"
+};
+
+export const expectedObjForPost : {name: string, job: string} = {
+    "name": "James",
+    "job": "AQA"
+};
+
+export const expectedObjForPut : {name: string, job: string} = {
+  "name": "Adam",
+  "job": "PM"
+};
+
+export const expectedObjForPatch : {name: string, job: string} = {
+  "name": "Kuba",
+  "job": "BA"
+};
--- HW_18/tests/jest-tests/api.spec.ts
+++ HW_18/tests/jest-tests/api.spec.ts
@@ -1,76 +1,12 @@
 import superAgent from "superagent";
-
-//переменные
-const expectedObjForGet : {id: number, email: string, first_name: string, last_name: string, avatar: string} = {
-    "id": 1,
-    "email": "george.bluth@reqres.in",
-    "first_name": "George",
-    "last_name": "Bluth",
-    "avatar": "https://reqres.in/img/faces/1-image.jpg"
-};
-
-const expectedObjForPost : {name: string, job: string} = {
-    "name": "James",
-    "job": "AQA"
-};
-
-const expectedObjForPut : {name: string, job: string} = {
-  "name": "Adam",
-  "job": "PM"
-};
-
-const expectedObjForPatch : {name: string, job: string} = {
-  "name": "Kuba",
-  "job": "BA"
-};
-
-
-//функции
-async function getApiResponse(url: string): Promise<any> {
-  try {
-    const response = await superAgent.get(url);
-    return response;
-  } catch (err: any) {
-    console.log(err.message);
-    return null;
-  }
-}
-
-
-async function postApiResponse(url: string, data: any): Promise<any> {
-  try {
-    const response = await superAgent
-      .post(url)
-      .set("Content-Type", "application/json")
-      .send(data);
-    return response;
-  } catch (err: any) {
-    console.log(err.message);
-    return null;
-  }
-}
-
-async function putApiResponse(url: string, data: any): Promise<any> {
-  try {
-    const response = await superAgent
-      .put(url)
-      .set("Content-Type", "application/json")
-      .send(data);
-    return response;
-  } catch (err: any) {
-    console.log(err.message);
-    return null;
-  }
-}
-
+import {expectedObjForGet, expectedObjForPost, expectedObjForPut, expectedObjForPatch} from "../../constants/variables";
+import {getApiResponse, postApiResponse, putApiResponse} from "../../constants/functions";
 
 
 //код, который нужно выполнить перед каждым тестом
 beforeEach(() => {
   }); 
   
-
-//тесты
 describe('api requests https://reqres.in/', () => {
   
   test('GET_SINGLE USER', async () => {
@@ -78,6 +14,13 @@ describe('api requests https://reqres.in/', () => {
     expect(response).not.toBeNull(); // Проверяем, что ответ не является null
     expect(response.status).toBe(200);
     expect(response.body.data).toEqual(expectedObjForGet);
+  });
+
+  test('SINGLE USER NOT FOUND', async () => {
+    const response = await getApiResponse("https://reqres.in/api/users/23");
+    expect(response.status).toBe(404);
+    expect(response.error).toBe('Not Found');
+    expect(response.body).toEqual({});
   });
 
   test('POST_CREATE', async () => {
